0aa805216ddc9fbad502726ca8311c9de8e74807
Itwerntme <72857681+Itwernme@users.noreply.github.com>
2023-01-19 21:10:06 +0000

    LDtk
    working on auto tiling in ldtk

--- scripts/LevelLoader.gd
+++ scripts/LevelLoader.gd
@@ -2,12 +2,13 @@ extends Node2D
 
 
 func _ready():
+	print(_read_json_file("res://assets/test.ldtk")["levels"])
 	pass
 
 func _parse_ldtk_level_file(var path : String):
 	_read_json_file(path)
 
-func _read_json_file(var path : String) -> JSONParseResult:
+func _read_json_file(var path : String) -> Dictionary:
 	var file = File.new()
 	file.open(path, file.READ)
 	var raw_json = file.get_as_text()
